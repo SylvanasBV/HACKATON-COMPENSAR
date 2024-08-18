ae40b101b79d051070203e17b6181623a324bf03
SylvanasBV <andres7.7mac@gmail.com>
2024-08-17 23:35:19 -0500

    Agrega archivo frontend desde SYLVANAS

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css';
+import Container from 'react-bootstrap/esm/Container';
+
+
+function Navbar() {
+  return (
+    <div className='navbar flex flex-row'>
+        <Container>
+            
+        </Container>
+    </div>
+  )
+}
+
+export default Navbar
