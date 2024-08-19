3db15f8207c17cbe536f97969e481463591647d4
SylvanasBV <andres7.7mac@gmail.com>
2024-08-19 07:08:08 -0500

    Se ingreso foros en landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Navbar from './components/navbar/Navbar';
 import News from './components/news/News';
+import Foros from './components/foros/Foros';
 
 
 
 function App() {
   return (
     <div className="App">
-        <Row className='p-0 m-0'>
+        <Col className='p-0 m-0'>
           <Navbar/>
         <div className='p-3'>
-          <News/>
+          <section className='pb-4'>
+            <News/>
+          </section>
+          <section>
+            <Foros/>
+          </section>
         </div>
-        </Row>
+        </Col>
     </div>
   );
 }
