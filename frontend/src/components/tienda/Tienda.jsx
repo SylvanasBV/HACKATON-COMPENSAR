15ebc1cc7bd43b56d13eba30774be4fb6966d55b
SylvanasBV <andres7.7mac@gmail.com>
2024-08-19 10:07:31 -0500

    Comienzo tienda

---
+++ frontend/src/components/tienda/Tienda.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Tienda.css'
+
+function Tienda() {
+  return (
+    <div className='tienda max-w-screen-xl flex flex-row flex-wrap items-center justify-between mx-auto'>
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default Tienda
